32f055e62c75a77718e8c5947dc81d57a3467718
_dream <will.ferens@gmail.com>
2018-04-08 10:05:56 -0600

    added saved recipes component, began building cards

--- components/SavedRecipes.js
+++ components/SavedRecipes.js
@@ -1,12 +1,49 @@
 import React, { Component } from "react"
-import { StyleSheet, View, Text, Picker, TextInput, Button, Modal } from "react-native"
+import { StyleSheet, View, Text, Picker, TextInput, Button, Modal, Image } from "react-native"
+import { Card, ListItem } from 'react-native-elements'
 
 export default class SavedRecipes extends Component {
+    
     render(){
+        let userSavedRecipes = this.props.navigation.state.params.currentUser.recipes
+        function breakUpUrl(rawRecipe){
+            var finalRecipeArray = []
+            for (var i = 0; i < rawRecipe.split(',').length-1; i++) {
+              var halfCookedRecipe = rawRecipe.split(',')[i]
+              var doneRecipe = halfCookedRecipe.split('$')
+              var secureImage = 'https' + doneRecipe[1].slice('4')
+              var nameArray = doneRecipe[3].split('_')
+              var newString = ''
+              for (let i=0;i<nameArray.length;i++) {
+                    newString = newString + " " + nameArray[i]
+              }
+              var eachRecipeObject = {
+                "id": doneRecipe[0],
+                "image": secureImage,
+                "url": doneRecipe[2],
+                "title": newString
+              }
+              finalRecipeArray.push(eachRecipeObject)
+            }
+            console.log(finalRecipeArray)
+            return finalRecipeArray
+          }
+          
         return(
-            <View>
-                <Text>{this.props.navigation.state.params.currentUser.userName}</Text>
-            </View>
+            <Card title="recipe">
+            {
+                breakUpUrl(userSavedRecipes).map((current, index) => {
+                    return (
+                        <View key={index} >
+                        <Image
+                            
+                        />
+                        <Text>{current.title}</Text>
+                        </View>
+                    )
+                })
+            }
+            </Card>
         )
     }
 }
