70b0c455933ae72a8e5fa89824ae751866457786
Seth Caparelli <sethcaparelli@gmail.com>
2018-04-08 22:30:25 -0600

    updated SavedRecipes

--- components/SavedRecipes.js
+++ components/SavedRecipes.js
@@ -12,68 +12,64 @@ export default class SavedRecipes extends Component {
 
     componentDidMount() {
         let id = this.props.navigation.state.params.currentUser.id
+        console.log(id)
         fetch("https://pure-meadow-62546.herokuapp.com/user/" + id)
         .then(response => {
-            // console.log(response)
+            console.log(response)
             return response.json()
         })
         .then(user => {
-            console.log(user.users.recipes)
-            let rawRecipe = user.users.recipes
+            console.log(user.users.recipe)
+            this.setState({
+                userSavedRecipes: user.users.recipes
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
+    render(){
+        function breakUpUrl(rawRecipe){
             var finalRecipeArray = []
             for (var i = 0; i < rawRecipe.split(',').length-1; i++) {
-                var halfCookedRecipe = rawRecipe.split(',')[i]
-                var doneRecipe = halfCookedRecipe.split('$')
-                var secureImage = 'https' + doneRecipe[1].slice('4')
-                var nameArray = doneRecipe[3].split('_')
-                var newString = ''
-                for (let i=0;i<nameArray.length;i++) {
+              var halfCookedRecipe = rawRecipe.split(',')[i]
+              var doneRecipe = halfCookedRecipe.split('$')
+              var secureImage = 'https' + doneRecipe[1].slice('4')
+              var nameArray = doneRecipe[3].split('_')
+              var newString = ''
+              for (let i=0;i<nameArray.length;i++) {
                     newString = newString + " " + nameArray[i]
-                }
-                var eachRecipeObject = {
+              }
+              var eachRecipeObject = {
                 "id": doneRecipe[0],
                 "image": secureImage,
                 "url": doneRecipe[2],
                 "title": newString
-                }
-                finalRecipeArray.push(eachRecipeObject)
+              }
+              finalRecipeArray.push(eachRecipeObject)
             }
             console.log(finalRecipeArray)
-            this.setState({
-                userSavedRecipes: finalRecipeArray
-            })
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
-
-    render(){
-        // let userSavedRecipes = this.props.navigation.state.params.currentUser.recipes
-        // let userSavedRecipes = this.state.userSavedRecipes
+            return finalRecipeArray
+          }
         return(
             <ScrollView>
-                <Text>High</Text>
-            {/* {
-            {/* this.state.userSavedRecipes.map((current, index) => {
+            {breakUpUrl(this.state.userSavedRecipes).map((current, index) => {
                 let url = current.url
                 return (
                     <Card
                     title={current.title}>
                         <View key={index} >
-                            <TouchableHighlight
-                            style={{justifyContent: "center", alignItems: "center"}}
-                            onPress={() => Linking.openURL(url)}>
-                            <Image
-                                source={{uri: current.image}}
-                                style={{height: 250, width: 250, borderRadius: 75}}
-                            />
-                            </TouchableHighlight>
+                        <TouchableHighlight
+                        style={{justifyContent: "center", alignItems: "center"}}
+                        onPress={() => Linking.openURL(url)}>
+                        <Image
+                            source={{uri: current.image}}
+                            style={{height: 250, width: 250, borderRadius: 75}}
+                        />
+                        </TouchableHighlight>
                         </View>
-                    </Card> */}
+                    </Card>
                 )
-            })
-        } */}
+            })}
         </ScrollView>
         )
     }
