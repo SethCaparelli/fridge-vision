e9b28f7b08c17be91cbcfecf27fa7e699a1fa9f2
Seth Caparelli <sethcaparelli@gmail.com>
2018-04-08 21:19:34 -0600

    reworking savedRecipes

--- components/SavedRecipes.js
+++ components/SavedRecipes.js
@@ -3,52 +3,77 @@ import { StyleSheet, View, Text, Picker, TextInput, Button, Modal, Image, Scroll
 import { Card, ListItem } from 'react-native-elements'
 
 export default class SavedRecipes extends Component {
-    render(){
-        let userSavedRecipes = this.props.navigation.state.params.currentUser.recipes
-        function breakUpUrl(rawRecipe){
+    constructor(props) {
+        super(props)
+        this.state = {
+            userSavedRecipes: ""
+        }
+    }
+
+    componentDidMount() {
+        let id = this.props.navigation.state.params.currentUser.id
+        fetch("https://pure-meadow-62546.herokuapp.com/user/" + id)
+        .then(response => {
+            // console.log(response)
+            return response.json()
+        })
+        .then(user => {
+            console.log(user.users.recipes)
+            let rawRecipe = user.users.recipes
             var finalRecipeArray = []
             for (var i = 0; i < rawRecipe.split(',').length-1; i++) {
-              var halfCookedRecipe = rawRecipe.split(',')[i]
-              var doneRecipe = halfCookedRecipe.split('$')
-              var secureImage = 'https' + doneRecipe[1].slice('4')
-              var nameArray = doneRecipe[3].split('_')
-              var newString = ''
-              for (let i=0;i<nameArray.length;i++) {
+                var halfCookedRecipe = rawRecipe.split(',')[i]
+                var doneRecipe = halfCookedRecipe.split('$')
+                var secureImage = 'https' + doneRecipe[1].slice('4')
+                var nameArray = doneRecipe[3].split('_')
+                var newString = ''
+                for (let i=0;i<nameArray.length;i++) {
                     newString = newString + " " + nameArray[i]
-              }
-              var eachRecipeObject = {
+                }
+                var eachRecipeObject = {
                 "id": doneRecipe[0],
                 "image": secureImage,
                 "url": doneRecipe[2],
                 "title": newString
-              }
-              finalRecipeArray.push(eachRecipeObject)
+                }
+                finalRecipeArray.push(eachRecipeObject)
             }
             console.log(finalRecipeArray)
-            return finalRecipeArray
-          }
+            this.setState({
+                userSavedRecipes: finalRecipeArray
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    render(){
+        // let userSavedRecipes = this.props.navigation.state.params.currentUser.recipes
+        // let userSavedRecipes = this.state.userSavedRecipes
         return(
             <ScrollView>
-            {
-            breakUpUrl(userSavedRecipes).map((current, index) => {
+                <Text>High</Text>
+            {/* {
+            {/* this.state.userSavedRecipes.map((current, index) => {
                 let url = current.url
                 return (
                     <Card
                     title={current.title}>
                         <View key={index} >
-                        <TouchableHighlight
-                        style={{justifyContent: "center", alignItems: "center"}}
-                        onPress={() => Linking.openURL(url)}>
-                        <Image
-                            source={{uri: current.image}}
-                            style={{height: 250, width: 250, borderRadius: 75}}
-                        />
-                        </TouchableHighlight>
+                            <TouchableHighlight
+                            style={{justifyContent: "center", alignItems: "center"}}
+                            onPress={() => Linking.openURL(url)}>
+                            <Image
+                                source={{uri: current.image}}
+                                style={{height: 250, width: 250, borderRadius: 75}}
+                            />
+                            </TouchableHighlight>
                         </View>
-                    </Card>
+                    </Card> */}
                 )
             })
-        }
+        } */}
         </ScrollView>
         )
     }
