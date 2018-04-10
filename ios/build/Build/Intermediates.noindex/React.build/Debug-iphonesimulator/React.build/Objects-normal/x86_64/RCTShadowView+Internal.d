23f75f8d399f3bda3907c31e7755d9d7742bb7f5
Seth Caparelli <sethcaparelli@gmail.com>
2018-04-09 20:20:11 -0600

    ran react-native upgrade

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTShadowView+Internal.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTShadowView+Internal.d
@@ -1,14 +1,14 @@
 dependencies: \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView+Internal.m \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView+Layout.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTComponent.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTRootView.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridge.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridgeDelegate.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTJavaScriptLoader.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridgeModule.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTInvalidating.h \
-  /Users/seth/workspace/fridgely/Fridge-Vision/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView+Internal.m \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView+Layout.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTShadowView.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Views/RCTComponent.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTRootView.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridge.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridgeDelegate.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTJavaScriptLoader.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTDefines.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTBridgeModule.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTFrameUpdate.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/node_modules/react-native/React/Base/RCTInvalidating.h \
+  /Users/seth/workspace/my-apps/fridgely/Fridge-Vision/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h
